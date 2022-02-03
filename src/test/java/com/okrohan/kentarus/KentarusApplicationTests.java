c25726b296270c83b04f9a7352a99a55485de0a3
okrohan <rohans16996@gmail.com>
2022-02-03 23:18:42 +0530

    feat: added spring starter

---
+++ src/test/java/com/okrohan/kentarus/KentarusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.okrohan.kentarus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KentarusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
